823ad5d2cfcb3ca1421bfeebe73ff310ee5ef323
catfisher1211 <cadentryon121@outlook.com>
2025-09-01 17:36:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+(function(){
+  const els = document.querySelectorAll('.reveal');
+  const obs = new IntersectionObserver(entries => {
+    entries.forEach(e => { if(e.isIntersecting){ e.target.classList.add('show'); obs.unobserve(e.target); } });
+  }, {threshold: .15});
+  els.forEach(el => obs.observe(el));
+})();
